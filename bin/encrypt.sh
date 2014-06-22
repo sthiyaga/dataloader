08e7a39f61cfa54b9916c4a303dd58c3014a7f7f
Senthil Thiyagarajan <xsthiyaga@yahoo.com>
2014-06-22 11:40:41 -0400

    Updated scripts

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -2,5 +2,9 @@
 
 JAVA=`which java`
 
-$JAVA -cp dataloader-30.0.0-uber.jar com.salesforce.dataloader.security.EncryptionUtil "$@" | sed 's/^.*) \- //g'	
-
+if [ "$JAVA" == "" ]; then
+        echo "Unable to find Java Runtime in PATH";
+        exit -1;
+else
+	$JAVA -cp dataloader-30.0.0-uber.jar com.salesforce.dataloader.security.EncryptionUtil "$@" | sed 's/^.*) \- //g'
+fi
